c2d961fc10c3090d243dfaeef108ddb337d96c73
Zhorabek <62075655+Zhorabek@users.noreply.github.com>
2021-02-08 11:33:14 +0600

    Add files via upload

---
+++ returnTrue.php
@@ -0,0 +1,16 @@
+/*This is an introduction to how challenges on Edabit work. In the Code tab above you'll see a starter function that looks like this:
+
+function returnTrue() {
+
+}
+All you have to do is type return true; between the curly braces { } and then click the Check button. If you did this correctly, the button will turn red and say SUBMIT FINAL. Click it and see what happens.
+Notes
+Don't forget to end lines with a ; semicolon.
+If you get stuck on a challenge, find help in the Resources tab.
+If you're really stuck, unlock solutions in the Solutions tab.*/
+
+
+//Answer:
+function returnTrue() {
+	return true;
+}
